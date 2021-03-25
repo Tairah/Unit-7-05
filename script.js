7b0b0ea1c8825c4fc854cb2e0aa756d3fe384b64
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-25 18:56:04 +0000

    fixed errors.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let userinput = 0
-let day = "saturday"
+let day = 'saturday'
 
 document.getElementById('submit').addEventListener('click', input)
 function input () {
   userinput = document.getElementById('Age').value
   day = document.getElementById('Day').value
-  if (userinput < 18 && (day != 'monday' || day  != 'wednesday')) {
-  alert('Have a great day at school')
- } else if (userinput > 18 && (day != 'Tuesday' || day  != 'Friday')) {
-  alert('Have a great day at work')
- }
+  if (userinput < 18 && (day !== 'monday' || day !== 'wednesday')) {
+    alert('Have a great day at school')
+  } else if (userinput > 18 && (day !== 'Tuesday' || day !== 'Friday')) {
+    alert('Have a great day at work')
+  }
 }
